a0a65cabd7cc66b7a6966b582ad3bf4d14589b01
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2024-03-09 18:00:23 -0500

    Add Select component

---
+++ src/shared/Select.jsx
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+import "../styles/shared/Select.css";
+import { useState } from "react";
+
+export const Select = ({options, selected}) => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="select">
+      <div className="select__item" onClick={() => setIsOpen(!isOpen)}>
+        <p>{selected}</p>
+        <svg
+          width="11"
+          height="7"
+          viewBox="0 0 11 7"
+          fill="none"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M5.25 7L6.11959e-07 -9.17939e-07L10.5 0L5.25 7Z"
+            fill="#333333"
+          />
+        </svg>
+      </div>
+      <div className={`select__options ${isOpen &&'select__options--open'}`}>
+        {options.map(({value, label}) => {
+          return (
+            <div className="select__item" key={value}>
+              {label}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+Select.propTypes = {
+  selected: PropTypes.string.isRequired,
+  options: PropTypes.array.isRequired,
+};
