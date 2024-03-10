a1fe8887b19fa9877554a91567d0b9468df535a8
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2024-03-10 15:01:34 -0500

    Add data to Card and GaugeBar components

--- src/shared/Card.jsx
+++ src/shared/Card.jsx
@@ -1,10 +1,10 @@
-import PropTypes from 'prop-types';
-import '../styles/shared/Card.css'
-import { GaugeBar } from './GaugeBar'
-import { Thumb } from './Thumb'
+import PropTypes from "prop-types";
+import "../styles/shared/Card.css";
+import { GaugeBar } from "./GaugeBar";
+import { Thumb } from "./Thumb";
 
-export const Card = ({celebrity}) => {
-  const {name, description, category, picture, lastUpdated} = celebrity;
+export const Card = ({ celebrity }) => {
+  const { name, description, category, picture, lastUpdated, votes } = celebrity;
 
   const calculateDiferenceDates = (dateString) => {
     const currentDate = new Date();
@@ -15,37 +15,43 @@ export const Card = ({celebrity}) => {
     const diffInYears = Math.floor(diffInDays / 365);
 
     if (diffInYears > 0) {
-      return `${diffInYears} year${diffInYears > 1 ? 's' : ''} ago`;
+      return `${diffInYears} year${diffInYears > 1 ? "s" : ""} ago`;
     }
     if (diffInMonths > 0) {
-      return `${diffInMonths} month${diffInMonths > 1 ? 's' : ''} ago`;
-    } 
-    return `${diffInDays} day${diffInDays > 1 ? 's' : ''} ago`;
-  }
+      return `${diffInMonths} month${diffInMonths > 1 ? "s" : ""} ago`;
+    }
+    return `${diffInDays} day${diffInDays > 1 ? "s" : ""} ago`;
+  };
 
   return (
     <div className="voting-card">
-      <img className="voting-card__portrait" src={`./assets/img/${picture}`} alt="Kanye West" />
-      <div className='voting-card__content'>
+      <img
+        className="voting-card__portrait"
+        src={`./assets/img/${picture}`}
+        alt="Kanye West"
+      />
+      <div className="voting-card__content">
         <div className="voting-card__data">
-          <div className='voting-card__thumb-average'>
-            <Thumb type="down" />
+          <div className="voting-card__thumb-average">
+            <Thumb type={votes.positive > votes.negative ? 'up' : 'down'} />
           </div>
           <h4>{name}</h4>
           <p>{description}</p>
         </div>
-        <div className='votin-card__votes'>
-          <p>{calculateDiferenceDates(lastUpdated)} in <span>{category}</span></p>
+        <div className="votin-card__votes">
+          <p>
+            {calculateDiferenceDates(lastUpdated)} in <span>{category}</span>
+          </p>
           <Thumb type="up" />
           <Thumb type="down" />
           <button>Vote now</button>
         </div>
-        <GaugeBar />
+        <GaugeBar votes={votes}/>
       </div>
     </div>
-  )
-}
+  );
+};
 
 Card.propTypes = {
-  celebrity: PropTypes.object.isRequired
-}
+  celebrity: PropTypes.object.isRequired,
+};
--- src/shared/GaugeBar.jsx
+++ src/shared/GaugeBar.jsx
@@ -1,20 +1,34 @@
+import PropTypes from "prop-types";
 import '../styles/shared/GaugeBar.css';
 
-export const GaugeBar = () => {
+export const GaugeBar = ({votes}) => {
+  const { positive, negative } = votes;
+  const calculatePercentage = () => {
+    const total = positive + negative;
+    return {
+      positive: `${(positive / total * 100).toFixed(1)}%`,
+      negative: `${(negative / total * 100).toFixed(1)}%`,
+    }
+  }
+
   return (
     <div className="gauge-bar">
-       <div style={{flex: `1 0 25.5%`}} className='gauge-bar__data gauge-bar__data--likes'>
+       <div style={{flex: `1 0 ${calculatePercentage().positive}`}} className='gauge-bar__data gauge-bar__data--likes'>
           <div className='gauge-bar__data__content'>
             <img src="./assets/img/thumbs-up.svg" alt="thumbs-up" />
-            <p>25.5%</p>
+            <p>{calculatePercentage().positive}</p>
           </div>
        </div>
-       <div style={{flex: `1 0 74.5%`}} className='gauge-bar__data gauge-bar__data--dislikes'>
+       <div style={{flex: `1 0 ${calculatePercentage().negative}`}} className='gauge-bar__data gauge-bar__data--dislikes'>
           <div className='gauge-bar__data__content'>
-            <p>74.5%</p>
+            <p>{calculatePercentage().negative}</p>
             <img src="./assets/img/thumbs-down.svg" alt="thumbs-down" />
           </div>
        </div>
     </div>
   )
 }
+
+GaugeBar.propTypes = {
+  votes: PropTypes.object.isRequired,
+};
